dd002c22e548fd24782c3e7745afbf0340a36875
iamsitting <cdsalamanca27@gmail.com>
2018-06-25 08:07:01 -0700

    smart menu & cards

--- ap/semi/utils.py
+++ ap/semi/utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from terms.models import Term
+
 ROLL_STATUS = (
     ('A', 'Attended'),
     ('S', 'Service'),
@@ -25,3 +28,13 @@ def attendance_stats(semi):
     d[status.split(' ')[0]] = count(att, code)
   d['Absences'] = 5 - d['Attended']
   return d
+
+
+def location_form_available():
+  week = Term.current_term().term_week_of_date(datetime.now())
+  return week >= 17 or week <= 19
+
+
+def attendance_form_available():
+  week = Term.current_term().term_week_of_date(datetime.now())
+  return week >= 18 or week <= 19
